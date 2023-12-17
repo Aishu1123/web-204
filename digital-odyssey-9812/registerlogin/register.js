9f002390b47056c3c4a5cdc0588ab57e54f329b5
Aishu1123 <147596468+Aishu1123@users.noreply.github.com>
2023-12-18 03:42:22 +0530

    CW Project
    ..

---
+++ digital-odyssey-9812/registerlogin/register.js
@@ -0,0 +1,49 @@
+
+
+
+let registerButton = document.getElementById("reg");
+registerButton.addEventListener("click", registerUser);
+
+document.getElementById('log').addEventListener('click', function(event) {
+  
+});
+
+
+
+function registerUser(event) {
+    event.preventDefault(); 
+
+    const username = document.getElementById('regusername').value;
+    const email = document.getElementById('regmail').value;
+    const contact = document.getElementById('regcontact').value;
+    const password = document.getElementById('regpassword').value;
+    const confirmpassword=document.getElementById('regconfirmpass').value;
+
+
+    if (username === '' || password === '' || email===''|| contact===''||password===''||confirmpassword==='') {
+        alert('Please enter all the details');
+        return;
+    }
+    
+  if(confirmpassword!==password){
+    alert ('password does not match');
+    return;
+  }
+
+
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        alert('Username already exists');
+        return;
+    }
+
+    users.push({ username, email, contact, password });
+
+    localStorage.setItem('users', JSON.stringify(users));
+
+    alert('Registration successful!');
+
+    window.location.href = 'login.html';
+}
